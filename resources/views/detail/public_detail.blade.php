9b25484296fdfd449f37104845c840817fee4084
Muhammad Rizki Dalfi <muhammad.rizki3405@student.unri.ac.id>
2025-05-27 17:54:46 +0700

    fix feature : add modal

--- resources/views/detail/public_detail.blade.php
+++ resources/views/detail/public_detail.blade.php
@@ -282,6 +282,118 @@
     @endif
 
     <div class="modal-group">
+        <div class="modal fade" id="familyModal" tabindex="-1" aria-hidden="true">
+            <div class="modal-dialog modal-lg" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel3">Tambah Anggota Keluarga</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <form action="{{ route('anggota.keluarga.store') }}" method="POST" enctype="multipart/form-data">
+                        @csrf
+                        <div class="modal-body">
+                            @if ($trah->id)
+                                <input type="hidden" name="tree_id" value="{{ $trah->id }}">
+                            @endif
+                            <div class="row">
+                                <div class="col mb-4">
+                                    <label for="nama_anggota_keluarga" class="form-label">Nama</label>
+                                    <input type="text" id="nama_anggota_keluarga" name="nama_anggota_keluarga"
+                                        class="form-control" placeholder="Nama Lengkap" required>
+                                </div>
+                            </div>
+                            <div class="row g-4">
+                                <div class="col mb-4">
+                                    <label for="jenis_kelamin" class="form-label">Jenis Kelamin</label>
+                                    <select id="jenis_kelamin" name="jenis_kelamin" class="form-select" required>
+                                        <option value="Laki-laki">Laki-laki</option>
+                                        <option value="Perempuan">Perempuan</option>
+                                    </select>
+                                </div>
+                                <div class="col mb-0">
+                                    <label for="tanggal_lahir" class="form-label">Tanggal Lahir</label>
+                                    <input type="date" id="tanggal_lahir" name="tanggal_lahir" class="form-control">
+                                </div>
+                            </div>
+                            <div class="row g-4">
+                                <div class="col mb-4">
+                                    <label for="parent_id" class="form-label">Orang Tua</label>
+                                    <select id="parent_id" name="parent_id" class="form-select">
+                                        <option value="">Pilih Orang Tua</option> <!-- Default empty option -->
+                                        @foreach ($anggota_keluarga as $member)
+                                            <option value="{{ $member->id }}"
+                                                {{ old('parent_id') == $member->id ? 'selected' : '' }}>
+                                                {{ $member->nama }}
+                                                @if ($member->jenis_kelamin === 'Laki-Laki')
+                                                    (Pak)
+                                                @else
+                                                    (Ibu)
+                                                @endif
+                                            </option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                <div class="col mb-0">
+                                    <label for="urutan" class="form-label">Urutan</label>
+                                    <select id="urutan" name="urutan" class="form-select" required>
+                                        <option value="1">1</option>
+                                        <option value="2">2</option>
+                                        <option value="3">3</option>
+                                        <option value="4">4</option>
+                                        <option value="5">5</option>
+                                        <option value="6">6</option>
+                                        <option value="7">7</option>
+                                        <option value="8">8</option>
+                                        <option value="9">9</option>
+                                        <option value="10">10</option>
+                                        <option value="11">11</option>
+                                        <option value="12">12</option>
+                                        <option value="13">13</option>
+                                        <option value="14">14</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="row g-4">
+                                <div class="col mb-4">
+                                    <label for="status_kehidupan" class="form-label">Status Kehidupan</label>
+                                    <select id="status_kehidupan" name="status_kehidupan" class="form-select" required>
+                                        <option value="Hidup">Hidup</option>
+                                        <option value="Wafat">Wafat</option>
+                                    </select>
+                                </div>
+                                <div class="col mb-0">
+                                    <label for="tanggal_kematian" class="form-label">Tanggal Kematian</label>
+                                    <input type="date" id="tanggal_kematian" class="form-control"
+                                        name="tanggal_kematian">
+                                </div>
+                            </div>
+                            <div class="row g-4">
+                                <div class="col mb-4">
+                                    <label for="alamat" class="form-label">Alamat</label>
+                                    <textarea class="form-control" name="alamat" required></textarea>
+                                </div>
+                            </div>
+                            <div class="row g-4">
+                                <div class="col mb-4">
+                                    <div class="mb-3">
+                                        <label for="keluarga_image" class="form-label">Upload Avatar</label>
+                                        <img src="" class="img-thumbnail image-preview mb-3"
+                                            style="display: none; max-width: 100px; max-height: 100px; object-fit: cover; aspect-ratio: 1/1; border-radius: 10px;">
+                                        <input class="form-control file-uploader" type="file" id="keluarga_image"
+                                            name="keluarga_image" accept="image/*" onchange="upload()">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-label-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary">Save changes</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
         @foreach ($anggota_keluarga as $anggota)
             <div class="modal fade" id="editMemberModal{{ $anggota->id }}" tabindex="-1" aria-hidden="true">
                 <div class="modal-dialog modal-lg" role="document">
--- resources/views/partials/family-member.blade.php
+++ resources/views/partials/family-member.blade.php
@@ -1,36 +1,30 @@
 <li>
     <a href="#" data-bs-toggle="modal" data-bs-target="#PopUpModal{{ $member->id }}">
-        <div class="absolute">{{ $member->urutan }}</div>
-        @if ($member->photo)
-            <div class="d-flex">
-                <!-- Foto Anggota -->
-                <img src="{{ asset('storage/' . $member->photo) }}" alt="Foto Anggota"
+        <small
+            style="
+            position: absolute; 
+            background-color: {{ $member->jenis_kelamin == 'Laki-Laki' ? '#3b82f6' : '#ec4899' }};
+            color: white;
+            font-weight: bold;
+            padding: 3px 8px; 
+            border-radius: 9999px; 
+            box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+            font-size: 0.75rem;
+            line-height: 1;
+            min-width: 24px;
+            text-align: center;
+            z-index: 100;
+            ">{{ $member->urutan }}
+        </small>
+        <div class="d-flex">
+           @if ($member->jenis_kelamin == 'Laki-Laki')
+                <img src="{{ asset('assets/img//placeholder/male.png') }}" alt="Foto Default Laki-laki"
                     style="width: 50px; height: 50px; object-fit: cover; border-radius: 50%;">
-
-                <!-- Foto Pasangan atau Foto Default -->
-                @if ($member->partner_photo)
-                    <img src="{{ asset('storage/' . $member->partner_photo) }}" alt="Foto Pasangan"
-                        style="width: 40px; height: 40px; object-fit: cover; border-radius: 50%;">
-                @else
-                    @if ($member->gender == 'Laki-Laki')
-                        <img src="{{ asset('../img/female.png') }}" alt="Foto Default Laki-laki"
-                            style="width: 50px; height: 50px; object-fit: cover; border-radius: 50%;">
-                    @elseif ($member->gender == 'Perempuan')
-                        <img src="{{ asset('../img/male.png') }}" alt="Foto Default Perempuan"
-                            style="width: 50px; height: 50px; object-fit: cover; border-radius: 50%;">
-                    @endif
-                @endif
-            </div>
-        @else
-            <!-- Jika tidak ada foto anggota, tampilkan foto default berdasarkan gender -->
-            @if ($member->gender == 'Laki-Laki')
-                <img src="{{ asset('../img/male.png') }}" alt="Foto Default Laki-laki"
-                    style="width: 50px; height: 50px; object-fit: cover; border-radius: 50%;">
-            @elseif ($member->gender == 'Perempuan')
-                <img src="{{ asset('../img/female.png') }}" alt="Foto Default Perempuan"
+            @elseif ($member->jenis_kelamin == 'Perempuan')
+                <img src="{{ asset('assets/img//placeholder/female.png') }}" alt="Foto Default Perempuan"
                     style="width: 50px; height: 50px; object-fit: cover; border-radius: 50%;">
             @endif
-        @endif
+        </div>
         <span class="capitalize">{{ $member->nama }}</span>
     </a>
 
@@ -45,7 +39,49 @@
 
     <div class="modal fade" id="PopUpModal{{ $member->id }}" tabindex="0"
         aria-labelledby="PopUpModal{{ $member->id }}" aria-hidden="true">
-        <div class="modal-dialog">
+        <div class="modal-dialog modal-dialog-centered text-center d-flex justify-content-center">
+            <div class="modal-content">
+            <div class="modal-body p-4">
+                @if ($member->jenis_kelamin == 'Laki-Laki')
+                    <img src="{{ asset('assets/img//placeholder/male.png') }}" alt="Foto Default Laki-laki"
+                        class="rounded-circle position-absolute top-0 start-50 translate-middle h-75">
+                @elseif ($member->jenis_kelamin == 'Perempuan')
+                    <img src="{{ asset('assets/img//placeholder/female.png') }}" alt="Foto Default Perempuan"
+                        class="rounded-circle position-absolute top-0 start-50 translate-middle h-75">
+                @endif
+                <div>
+                    <h5 class="pt-5 my-3">{{ $member->nama }}</h5>
+                    <label class="form-label d-block" for="tanggal_lahir">Lahir Pada {{ \Carbon\Carbon::parse($member->tanggal_lahir)->format('d-m-Y') }}</label>
+                </div>
+
+                @if ($member->partners->count() > 0)
+                                <p>
+                                    @foreach ($member->partners as $partner)
+                                        <a href="#" data-bs-toggle="modal"
+                                            data-bs-target="#PopUpModal{{ $partner->id }}">
+                                            {{ $partner->nama }}@if (!$loop->last)
+                                                ,
+                                            @endif
+                                        </a>
+                                    @endforeach
+                                </p>
+                            @endif
+                            @if ($member->children->count() > 0)
+                                <p>
+                                    @foreach ($member->children as $child)
+                                        <a href="#" data-bs-toggle="modal"
+                                            data-bs-target="#PopUpModal{{ $child->id }}">
+                                            {{ $child->nama }}@if (!$loop->last)
+                                                ,
+                                            @endif
+                                        </a>
+                                    @endforeach
+                                </p>
+                            @endif
+            </div>
+        </div>
+        </div>
+        {{-- <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">Biodata {{ $member->nama }}</h5>
@@ -101,25 +137,6 @@
                                 <p>Anak</p>
                             @endif
                         </div>
-                        {{-- <div class="col text-start">
-                            <p>{{ $member->nama }}</p>
-                            <p>{{ \Carbon\Carbon::parse($member->tanggal_lahir)->format('d-m-Y') }}</p>
-                            <p>{{ $member->jenis_kelamin }}</p>
-                           @if ($member->partners->count() > 0)
-                                <p>
-                                    @foreach ($member->partners as $partner)
-                                        {{ $partner->nama }}@if (!$loop->last), @endif
-                                    @endforeach
-                                </p>
-                            @endif
-                           @if ($member->children->count() > 0)
-                                <p>
-                                    @foreach ($member->children as $children)
-                                        {{ $children->nama }}@if (!$loop->last), @endif
-                                    @endforeach
-                                </p>
-                            @endif
-                        </div> --}}
                         <div class="col text-start">
                             <p>{{ $member->nama }}</p>
                             <p>{{ \Carbon\Carbon::parse($member->tanggal_lahir)->format('d-m-Y') }}</p>
@@ -156,6 +173,6 @@
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tutup</button>
                 </div>
             </div>
-        </div>
+        </div> --}}
     </div>
 </li>
